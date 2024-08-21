b8fa8492e2b9fd4e24d50b43fbb493c9a4094d55
AmitAcco <163759810+AmitAcco@users.noreply.github.com>
2024-08-21 18:28:19 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 const splashScreen = document.getElementById('splashScreen');
 const flashlight = document.getElementById('flashlight');
 const toggleButton = document.getElementById('toggleButton');
+const closeButton = document.getElementById('closeButton');
 const line1 = document.querySelector('.line1');
 let isFlashing = false;
 let flashInterval;
-let backPressCount = 0;
 
 // Function to start flashing
 function flashScreen() {
     flashlight.style.display = 'block';
     toggleButton.style.display = 'block';
+    flashlight.style.backgroundColor = 'blue';
     line1.classList.add('flash'); // Add flash class to start text color animation
     flashInterval = setInterval(() => {
         flashlight.style.backgroundColor = flashlight.style.backgroundColor === 'blue' ? 'red' : 'blue';
-    }, 1300);
+    }, 450);
 }
 
 // Function to stop flashing
@@ -35,23 +36,30 @@ toggleButton.addEventListener('click', () => {
     isFlashing = !isFlashing;
 });
 
-// Handle back button press on Android devices
-document.addEventListener('backbutton', () => {
-    backPressCount += 1;
-    if (backPressCount === 2) {
-        if (window.Android) {
-            window.Android.closeWebView(); // Call Android method to close WebView
-        }
+// Event listener for the close button
+closeButton.addEventListener('click', () => {
+    if (window.Android) {
+        window.Android.closeWebView(); // Call Android method to close WebView
     } else {
-        alert('Click back twice to close the application');
-        setTimeout(() => { backPressCount = 0; }, 2000);
+        window.close(); // Attempt to close the window
     }
-}, false);
+});
 
 // Function to handle splash screen touch
 function handleSplashScreenTouch() {
     splashScreen.style.display = 'none';
     toggleButton.style.display = 'block';
+    closeButton.style.display = 'block';
+}
+
+// Add touch event listener to splash screen
+splashScreen.addEventListener('click', handleSplashScreenTouch);
+splashScreen.addEventListener('touchstart', handleSplashScreenTouch);
+function handleSplashScreenTouch(event) {
+    console.log("Splash screen touched or clicked"); // Debugging statement
+    splashScreen.style.display = 'none';
+    toggleButton.style.display = 'block';
+    closeButton.style.display = 'block';
 }
 
 // Add touch event listener to splash screen
